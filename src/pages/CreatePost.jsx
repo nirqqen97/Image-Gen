06e04b64ff8cb53934913fcb357d2e1621d3cd15
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2024-01-28 01:08:32 +0200

    updates

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -11,7 +11,35 @@ const createPost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "https://dalle-arbb.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        );
+
+        await response.json();
+        alert("Success");
+        navigate("/");
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please generate an image with proper details");
+    }
+  };
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
@@ -23,17 +51,21 @@ const createPost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("htpps//localhost:8080/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "http://localhost:8080/api/v1/dalle/test",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (error) {
         alert(error);
+        console.log("error: ", error);
       } finally {
         setGeneratingImg(false);
       }
